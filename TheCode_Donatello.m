7a77fd109c54cb28640a274b5f58d6ca2341371d
Dotalon <maggidonatello14@yahoo.it>
2024-04-07 18:10:05 +0200

    New distributed structures
    added structures: Cyclic, Reinforced BiStar, Optimized

--- TheCode_Donatello.m
+++ TheCode_Donatello.m
@@ -124,7 +124,7 @@ spectral_abscissa = round(max(real(eig(A))),10)
 eigenvaluesDT = eig(F)
 spectral_radius = round(max(abs(eig(F))),10)
 
-%the system is simply stable as we have only one eig on the border (zero for CT , 1 for DT),
+%the open loop system is simply stable as we have only one eig on the border (zero for CT , 1 for DT),
 %in both cases
 
 %% state-feedback centralized control structures
@@ -149,7 +149,13 @@ feas_cent
 rho_dt_cent
 feas_dt_cent
 
-%CT H2 gains
+eigenvaluesCT = eig(A+B*K_cent)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_cent)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%CT and DT H2 gains
 [K_cent2,rho_cent2,feas_cent2]=LMI_CT_H2(A,Bdec,Cdec,N,ContStruc,Bw);
 rho_cent2
 feas_cent2
@@ -157,6 +163,11 @@ feas_cent2
 rho_dt_cent2
 feas_dt_cent2
 
+eigenvaluesCT = eig(A+B*K_cent2)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_cent2)
+spectral_radius = round(max(abs(eig(F))),10)
 
 %% state-feedback Decentralized control structure
 rounding_n=3;
@@ -185,6 +196,10 @@ rho_dt_dec
 feas_dt_dec
 
 %% state-feedback distributed control structures
+% In case one wants to create a sort of a hierarchy with a master subsystem
+% e.g. the one that produces most, or whose production is most important.
+% (in this case it's subsystem 1)
+
 %Bidirectional star topology
 ContStruc_BiStar= [ 1 1 1 1 1
                     1 1 0 0 0
@@ -209,16 +224,138 @@ K_dt_BiStar
 rho_dt_BiStar
 feas_dt_BiStar
 
+%% Reinforced Bi-Directional Star
+% Like Bi Star but we add extra connections to try improve the overall
+% stability.
+%Reinforced Bidirectional Star topology
+ContStruc_REBiStar= [ 1 1 1 1 1
+                      1 1 0 0 1
+                      1 1 1 0 0
+                      1 0 1 1 0
+                      1 0 0 1 1];
 
+CFM_REBiStar=di_fixed_modes(A,Bdec,Cdec,N,ContStruc_REBiStar, rounding_n)
+%no fixed modes in Continuous
+DFM_REBiStar=di_fixed_modes(F,Gdec,Hdec,N,ContStruc_REBiStar, rounding_n)
+%no fixed modes in Discrete
 
-%%
+%Continuous Time stabilizing gains
+[K_RE,rho_RE,feas_RE]=LMI_CT_DeDicont(A,Bdec,Cdec,N,ContStruc_REBiStar);
+rho_RE
+feas_RE
+%Discrete Time stabilizing gains
+[K_dt_RE,rho_dt_RE,feas_dt_RE]=LMI_DT_DeDicont(F,Gdec,Hdec,N,ContStruc_REBiStar);
+rho_dt_RE
+feas_dt_RE
 
-[ContStruc,K,best_rho,feas]=Optimize_ContStruc_CT_H2(A,Bdec,Cdec,N,Bw);
-ContStruc
-feas
-best_rho
+eigenvaluesCT = eig(A+B*K_RE)
+spectral_abscissa = round(max(real(eig(A))),10)
 
-[ContStrucd,Kd,best_rhod,feasd]=Optimize_ContStruc_DT_H2(F,Gdec,Hdec,N,Gw);
-ContStrucd
-feasd
-best_rhod
+eigenvaluesDT = eig(F+G*K_dt_RE)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%CT and DT H2 gains
+[K_RE2,rho_RE2,feas_RE2]=LMI_CT_H2(A,Bdec,Cdec,N,ContStruc_REBiStar,Bw);
+rho_RE2
+feas_RE2
+[K_dt_RE2,rho_dt_RE2,feas_dt_RE2]=LMI_DT_H2(F,Gdec,Hdec,N,ContStruc_REBiStar,Gw);
+rho_dt_RE2
+feas_dt_RE2
+
+eigenvaluesCT = eig(A+B*K_RE2)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_RE2)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%% OPTIMIZED structure
+% doesnt make much sense graphically speaking. Apart from the abscissa, the
+% rest of the eig is in general worse than the Reinforced Bi Star
+% Nevertheless this implementation gets the best rho!
+
+%Reinforced Bidirectional Star topology
+ContStruc_OPT= [ 1 1 0 0 1
+                 0 1 1 0 0
+                 1 1 1 1 1
+                 0 0 0 1 0  %%PROBLEM: optimization outputs a matrix with no inputs on the 4th system! 
+                 1 0 0 1 1];
+
+CFM_OPT=di_fixed_modes(A,Bdec,Cdec,N,ContStruc_OPT, rounding_n)
+%no fixed modes in Continuous
+DFM_OPT=di_fixed_modes(F,Gdec,Hdec,N,ContStruc_OPT, rounding_n)
+%no fixed modes in Discrete
+
+%Continuous Time stabilizing gains
+[K_OPT,rho_OPT,feas_OPT]=LMI_CT_DeDicont(A,Bdec,Cdec,N,ContStruc_OPT);
+rho_OPT 
+feas_OPT
+%Discrete Time stabilizing gains
+[K_dt_OPT,rho_dt_OPT,feas_dt_OPT]=LMI_DT_DeDicont(F,Gdec,Hdec,N,ContStruc_OPT);
+rho_dt_OPT
+feas_dt_OPT
+
+eigenvaluesCT = eig(A+B*K_OPT)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_OPT)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%CT and DT H2 gains
+[K_OPT2,rho_OPT2,feas_OPT2]=LMI_CT_H2(A,Bdec,Cdec,N,ContStruc_OPT,Bw);
+rho_OPT2
+feas_OPT2
+[K_dt_OPT2,rho_dt_OPT2,feas_dt_OPT2]=LMI_DT_H2(F,Gdec,Hdec,N,ContStruc_OPT,Gw);
+rho_dt_OPT2
+feas_dt_OPT2
+
+eigenvaluesCT = eig(A+B*K_OPT2)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_OPT2)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%% Bidirectional Cyclic structure
+% for the total system this is the one that makes most sense in my opinion,
+% as there is no clear hierarchical division between the subsystem. In case
+% one wants to choose  a specific subsystem as the one with most
+% production, the Reinforced Bi Star is more suitable
+ContStruc_cyc= [ 1 1 0 0 1
+                 1 1 1 0 0
+                 0 1 1 1 0
+                 0 0 1 1 1   
+                 1 0 0 1 1];
+
+CFM_cyc=di_fixed_modes(A,Bdec,Cdec,N,ContStruc_cyc, rounding_n)
+%no fixed modes in Continuous
+DFM_cyc=di_fixed_modes(F,Gdec,Hdec,N,ContStruc_cyc, rounding_n)
+%no fixed modes in Discrete
+
+%Continuous Time stabilizing gains
+[K_cyc,rho_cyc,feas_cyc]=LMI_CT_DeDicont(A,Bdec,Cdec,N,ContStruc_cyc);
+rho_cyc
+feas_cyc
+%Discrete Time stabilizing gains
+[K_dt_cyc,rho_dt_cyc,feas_dt_cyc]=LMI_DT_DeDicont(F,Gdec,Hdec,N,ContStruc_cyc);
+rho_dt_cyc
+feas_dt_cyc
+
+eigenvaluesCT = eig(A+B*K_cyc)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_cyc)
+spectral_radius = round(max(abs(eig(F))),10)
+
+%CT and DT H2 gains
+[K_cyc2,rho_cyc2,feas_cyc2]=LMI_CT_H2(A,Bdec,Cdec,N,ContStruc_cyc,Bw);
+rho_cyc2
+feas_cyc2
+[K_dt_cyc2,rho_dt_cyc2,feas_dt_cyc2]=LMI_DT_H2(F,Gdec,Hdec,N,ContStruc_cyc,Gw);
+rho_dt_cyc2
+feas_dt_cyc2
+
+eigenvaluesCT = eig(A+B*K_cyc2)
+spectral_abscissa = round(max(real(eig(A))),10)
+
+eigenvaluesDT = eig(F+G*K_dt_cyc2)
+spectral_radius = round(max(abs(eig(F))),10)
+
