bf6367a679fa1068abff8c4b93cfff48cabb2e15
Dotalon <maggidonatello14@yahoo.it>
2024-04-08 11:08:46 +0200

    Create structures.m

---
+++ structures.m
@@ -0,0 +1,29 @@
+ContStruc_REBiStar= [ 1 1 1 1 1
+                      1 1 0 0 1
+                      1 1 1 0 0
+                      1 0 1 1 0
+                      1 0 0 1 1];
+
+ContStruc_OPT= [ 1 1 0 0 1
+                 0 1 1 0 0
+                 1 1 1 1 1
+                 0 0 0 1 0  %%PROBLEM: optimization outputs a matrix with no inputs on the 4th system! 
+                 1 0 0 1 1];
+
+ContStruc_cyc= [ 1 1 0 0 1
+                 1 1 1 0 0
+                 0 1 1 1 0
+                 0 0 1 1 1   
+                 1 0 0 1 1];
+
+ContStruc_inlfu= [ 1 1 0 0 0
+                   1 1 1 0 1
+                   0 1 1 1 0
+                   0 0 1 1 1   
+                   0 1 0 1 1];
+
+ContStruc_NotInflu= [ 1 0 1 1 1
+                      0 1 0 1 0
+                      1 0 1 0 1
+                      1 1 0 1 0   
+                      1 0 1 0 1]; 
