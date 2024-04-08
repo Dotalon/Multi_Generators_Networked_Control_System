d99061aa5da8d7c3606a926f27903a0ea445ebe3
Dotalon <maggidonatello14@yahoo.it>
2024-04-08 18:09:02 +0200

    Update structures.m

--- structures.m
+++ structures.m
@@ -17,13 +17,13 @@ ContStruc_cyc= [ 1 1 0 0 1
                  0 0 1 1 1   
                  1 0 0 1 1];
 
-ContStruc_inlfu= [ 1 1 0 0 0
-                   1 1 1 0 1
-                   0 1 1 1 0
-                   0 0 1 1 1   
-                   0 1 0 1 1];
+ContStruc_Pij= [ 1 1 0 0 0
+                 1 1 1 0 1
+                 0 1 1 1 0
+                 0 0 1 1 1   
+                 0 1 0 1 1];
 
-ContStruc_NotInflu= [ 1 0 1 1 1
+ContStruc_NotPij= [ 1 0 1 1 1
                       0 1 0 1 0
                       1 0 1 0 1
                       1 1 0 1 0   
